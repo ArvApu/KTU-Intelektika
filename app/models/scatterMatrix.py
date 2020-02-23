c9443daddda693bff1923b85552d7edde153777e
arvapu <arvydas.apulskis123@gmail.com>
2020-02-23 21:35:33 +0200

    minor improvments

--- app/models/scatterMatrix.py
+++ app/models/scatterMatrix.py
@@ -4,13 +4,13 @@ import pandas as pd
 import numpy as np
 
 
-def rotate_labels(axs,n):
+def rotate_labels(axs, n):
     for x in range(n):
         for y in range(n):
             # to get the axis of subplots
             ax = axs[x, y]
             # to make x axis name vertical
-            ax.xaxis.label.set_rotation(90)
+            ax.xaxis.label.set_rotation(45)
             # to make y axis name horizontal
             ax.yaxis.label.set_rotation(0)
             # to make sure y axis names are outside the plot area
@@ -26,7 +26,7 @@ class ScatterMatrix:
         axs = scatter_matrix(df, alpha=0.4, figsize=(19, 19), diagonal='hist', c='blue',
                              hist_kwds={'color': ['red']})
 
-        rotate_labels(axs,len(df.columns))
+        rotate_labels(axs, len(df.columns))
 
         plt.subplots_adjust(top=0.99, right=0.99)
         plt.show()
